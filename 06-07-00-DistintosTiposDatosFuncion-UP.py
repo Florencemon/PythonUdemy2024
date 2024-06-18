4f8577de5aa06a8f8966b5e6c9e4335a8bd94e8e
Florencia Chirino <florrominachirino@gmail.com>
2024-06-18 00:05:53 -0300

    6to commit

--- 06-07-00-DistintosTiposDatosFuncion-UP.py
+++ 06-07-00-DistintosTiposDatosFuncion-UP.py
@@ -7,3 +7,25 @@ desplegarNombres(nombres)
 desplegarNombres('Carlos')
 desplegarNombres((8, 9))
 desplegarNombres([10, 11])
+
+""""otra forma"""
+def desplegarNombres(nombres):
+    for nombre in nombres:
+        print(nombre)
+
+nombres=[]
+
+while True:
+
+    # Solicitar nombres al usuario y almacenarlos en una lista
+    entrada = input("Introduce un nombre y S para SALIR: ")
+
+    if entrada == "S":
+        print("adios")
+        print(nombres)
+        break
+    else:
+        nombres.append(entrada)
+
+    # Llamar a la función para desplegar los nombres
+    desplegarNombres([nombres])
